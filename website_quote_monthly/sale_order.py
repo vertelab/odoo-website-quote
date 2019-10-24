9e2cd7d8afa2f5f6495f9fa9259911507947ef55
Anders Wallenquist <anders.wallenquist@vertel.se>
2019-10-24 21:13:09 +0000

    website_quote_monthly: amount calculation

--- website_quote_monthly/sale_order.py
+++ website_quote_monthly/sale_order.py
@@ -30,12 +30,25 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit='sale.order'
     
-    @api.one
-    @api.depends('order_line')
+    @api.depends('order_line.price_total')
     def _compute_amount_month(self):
         month = self.env.ref('website_quote_template.product_uom_month')
-        self.order_line_month_ids = self.order_line.filtered(lambda x: x.product_id and x.product_id.uom_id == month)
-        self.order_line_fixed_ids = self.order_line.filtered(lambda x: x.product_id and x.product_id.uom_id != month)
+        for order in self:
+            order.order_line_month_ids = order.order_line.filtered(lambda x: x.product_id and x.product_id.uom_id == month)
+            order.order_line_fixed_ids = order.order_line.filtered(lambda x: x.product_id and x.product_id.uom_id != month)
+            amount_untaxed = amount_tax = 0.0   
+            for line in order.order_line:
+                if line.product_id and line.product_id.uom_id == month:
+                    amount_untaxed_month += line.price_subtotal
+                    amount_tax_month += line.price_tax
+                else:
+                    amount_untaxed_fixed += line.price_subtotal
+                    amount_tax_fixed += line.price_tax
+            order.update({
+                'amount_month_total': amount_untaxed_month,
+                'amount_fixed_total': amount_untaxed_fixed,
+            })
+        
     
     amount_month_total = fields.Float(compute='_compute_amount_month')
     amount_fixed_total = fields.Float(compute='_compute_amount_month')
