1c597ff754cae3db9c22efbdadc0860233c03b2b
Anders Wallenquist <anders.wallenquist@vertel.se>
2019-10-24 06:42:40 +0000

    website_quote_monthly: new fields for the template

--- website_quote_monthly/sale_order.py
+++ website_quote_monthly/sale_order.py
@@ -27,93 +27,19 @@ from odoo.exceptions import except_orm, Warning, RedirectWarning
 import logging
 _logger = logging.getLogger(__name__)
 
-# ~ class ProductTemplate(models.Model):
-    # ~ _inherit = 'product.template'
-
-    # ~ bootstrap_icon = fields.Char(string='Bootstrap Icon', help="eg fa fa-laptop")
-    # ~ description_quote = fields.Text(string="Description for quotations",translate=True,placeholder="note to be displayed on on-line quotations...", help="A description of the Product that you want to communicate to your customers on on-line quotes.") 
-
 class SaleOrder(models.Model):
     _inherit='sale.order'
     
-    order_line_normal = fields.One2many('sale.order.line', compute='_compute_order_line_normal')
-    order_line_normal_untaxed = fields.Float(compute='_compute_order_line_normal_untaxed')
-    order_line_normal_tax = fields.Float(compute='_compute_order_line_normal_tax')
-    order_line_normal_total = fields.Float(compute='_compute_order_line_normal_total')
-    order_line_normal_undiscounted = fields.Float(compute='_compute_order_line_normal_undiscounted')
-
-    order_line_monthly = fields.One2many('sale.order.line', compute='_compute_order_line_monthly')
-    order_line_monthly_untaxed = fields.Float(compute='_compute_order_line_monthly_untaxed')
-    order_line_monthly_tax = fields.Float(compute='_compute_order_line_monthly_tax')
-    order_line_monthly_total = fields.Float(compute='_compute_order_line_monthly_total')
-    order_line_monthly_undiscounted = fields.Float(compute='_compute_order_line_monthly_undiscounted')
+    @api.one
+    @api.depends('order_line')
+    def _compute_amount_month(self):
+        month = self.env.ref('website_quote_template.product_uom_month')
+        self.order_line_month_ids = self.order_line.filtered(lambda x: x.product_id and x.product_id.uom_id == month)
+        self.order_line_fixed_ids = self.order_line.filtered(lambda x: x.product_id and x.product_id.uom_id != month)
     
-    @api.one
-    @api.depends('order_line')
-    def _compute_order_line_monthly(self):
-        month = self.env.ref('website_quote_template.product_uom_month')
-        self.order_line_monthly = self.order_line.filtered(lambda x: x.product_id and x.product_id.uom_id == month)
-
-    @api.one
-    @api.depends('order_line_monthly')
-    def _compute_order_line_monthly_untaxed(self):
-        tot = 0.0
-        for line in self.order_line_monthly:
-            tot += line.price_subtotal
-        self.order_line_monthly_untaxed = tot
-
-    @api.one
-    @api.depends('order_line_monthly')
-    def _compute_order_line_monthly_tax(self):
-        tot = 0.0
-        for line in self.order_line_monthly:
-            tot += self._amount_line_tax(line)
-        self.order_line_monthly_tax = tot
-
-    @api.one
-    @api.depends('order_line_monthly_untaxed', 'order_line_monthly_tax')
-    def _compute_order_line_monthly_total(self):
-        self.order_line_monthly_total = self.order_line_monthly_untaxed + self.order_line_monthly_tax
-
-    @api.one
-    @api.depends('order_line')
-    def _compute_order_line_normal(self):
-        month = self.env.ref('website_quote_template.product_uom_month')
-        self.order_line_normal = self.order_line.filtered(lambda x: not (x.product_id and x.product_id.uom_id == month))
-
-    @api.one
-    @api.depends('order_line_normal')
-    def _compute_order_line_normal_untaxed(self):
-        tot = 0.0
-        for line in self.order_line_normal:
-            tot += line.price_subtotal
-        self.order_line_normal_untaxed = tot
-
-    @api.one
-    @api.depends('order_line_normal')
-    def _compute_order_line_normal_tax(self):
-        tot = 0.0
-        for line in self.order_line_normal:
-            tot += self._amount_line_tax(line)
-        self.order_line_normal_tax = tot
-
-    @api.one
-    @api.depends('order_line_normal_untaxed', 'order_line_normal_tax')
-    def _compute_order_line_normal_total(self):
-        self.order_line_normal_total = self.order_line_normal_untaxed + self.order_line_normal_tax
-
-    @api.one
-    @api.depends('order_line_normal', 'order_line_normal_tax')
-    def _compute_order_line_normal_undiscounted(self):
-        total = 0.0
-        for line in self.order_line_normal:
-            total += line.price_subtotal + line.price_unit * ((line.discount or 0.0) / 100.0) * line.product_uom_qty
-        self.order_line_normal_undiscounted = total
-
-    @api.one
-    @api.depends('order_line_monthly', 'order_line_monthly_tax')
-    def _compute_order_line_monthly_undiscounted(self):
-        total = 0.0
-        for line in self.order_line_monthly:
-            total += line.price_subtotal + line.price_unit * ((line.discount or 0.0) / 100.0) * line.product_uom_qty
-        self.order_line_monthly_undiscounted = total
+    amount_month_total = fields.Float(compute='_compute_amount_month')
+    amount_fixed_total = fields.Float(compute='_compute_amount_month')
+    order_line_month_ids = fields.One2many('sale.order.line', compute='_compute_amount_month')
+    order_line_fixed_ids = fields.One2many('sale.order.line', compute='_compute_amount_month')
+    
+ 
