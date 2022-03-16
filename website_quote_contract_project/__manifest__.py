b7eaba42d12b2cdc4db4491decdfcdae902fe656
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2022-03-16 15:40:35 +0000

    Added a field to be used in our contract sale order template

---
+++ website_quote_contract_project/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name':'Website quote contract project',
+    'author':'Vertel AB',
+    'category': 'Sales',
+    'licence': 'AGPL-3',
+    'version':'14.0.1.0.0',
+    'website': 'http://www.vertel.se',
+    'description': 'A help module that adds a field on a sale order that is the created project from a sale order, which is used by a specific sale order template',
+    'data': [
+        'views/sale_order_views.xml',
+        ],
+    'depends': ['sale','sale_quotation_builder','website_quote_header'],
+}
+
