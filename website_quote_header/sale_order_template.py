e1d836d08cf59e1685adfa532ef0d459331e4a4a
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2022-03-17 07:21:58 +0000

    Added default values, for html fields

--- website_quote_header/sale_order_template.py
+++ website_quote_header/sale_order_template.py
@@ -11,7 +11,7 @@ class SaleOrderTemplate(models.Model):
     website_description_footer = fields.Html('Website Description Footer', sanitize_attributes=False, translate=html_translate)
     terms_page = fields.Char('Terms Page')
     
-    header_template_description = fields.Html('Website Description header', sanitize_attributes=False, translate=html_translate) ####### WHAT IM GOING TO USE TO CALL THE T RAW WITH
-    footer_template_description = fields.Html('Website Description footer', sanitize_attributes=False, translate=html_translate) ####### WHAT IM GOING TO USE TO CALL THE T RAW WITH
+    header_template_description = fields.Html('Website Description header', sanitize_attributes=False, translate=html_translate, default="<p></p>") ####### WHAT IM GOING TO USE TO CALL THE T RAW WITH
+    footer_template_description = fields.Html('Website Description footer', sanitize_attributes=False, translate=html_translate, default="<p></p>") ####### WHAT IM GOING TO USE TO CALL THE T RAW WITH
 
 
