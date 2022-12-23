a7d3e19bcbb480d586f3b7737af5586b769161e6
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-12-23 14:03:41 +0100

    Update __manifest__.py

--- website_quote_header/__manifest__.py
+++ website_quote_header/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Website Quote:  Header',
+    'name': 'Website Quote: Header',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Fixed header styles in quote.',
