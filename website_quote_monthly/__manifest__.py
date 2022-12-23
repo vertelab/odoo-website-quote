06c97f316e93cb7e1c6e087c9beef451ccd482ed
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-12-23 11:00:26 +0100

    Update __manifest__.py

--- website_quote_monthly/__manifest__.py
+++ website_quote_monthly/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo, Open Source Enterprise Management Solution, third party addon
-#    Copyright (C) 2019 Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,17 +15,19 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    'name': 'Website Quote Template Monthly',
-    'author': 'Vertel AB',
-    'category': 'Sales',
-    'licence': 'AGPL-3',
+    'name': 'Website Quote:  Monthly, Template',
     'version': '14.0.1.0.0',
-    'website': 'https://www.vertel.se',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'More features for online quotation.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Sales',
     'description': """
 More features for online quotation
 ==================================
@@ -34,11 +36,21 @@ More features for online quotation
 * Product detail description for quotation
 * New unit of measure month
 * Additional table for monthly cost products
-""",
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-website-quote/website_quote_monthly',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-website-quote',
+    # Any module necessary for this one to work correctly
+
     'depends': ['sale_quotation_builder'],
     'data': [
         'data/data.xml',
         'views/template.xml',
     ],
 }
-# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
