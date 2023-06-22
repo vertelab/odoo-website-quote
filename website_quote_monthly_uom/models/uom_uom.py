12727d24053001e951ca4edf762a376edcbf7794
Dmitri Iselund <dmitri.iselund@gmail.se>
2023-06-22 13:55:19 +0200

    New 16.0 branch with website_quote_monthly ported

--- website_quote_monthly_uom/models/uom_uom.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, tools, models, _
-from odoo.exceptions import UserError, ValidationError
-
-
-class UoM_add_field(models.Model):
-    _inherit = 'uom.uom'
-
-    monthly_bool = fields.Boolean("Exist inside monthly sums")
-    
