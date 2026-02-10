faf0e0e177c0a6e7d468f232d7b5d740686bf3b9
iammiracle <miracleaayodele@gmail.com>
2026-02-10 08:45:32 +0100

    T/7326: dynamically render html content

--- website_quote_header/models/sale_order.py
+++ website_quote_header/models/sale_order.py
@@ -13,57 +13,42 @@ class SaleOrder(models.Model):
 
     header_template_description = fields.Html(
         'Website Description Header', sanitize_attributes=False, translate=html_translate, default="<p></p>")
-    website_description_footer = fields.Html(
-        'Website Description', sanitize_attributes=False, translate=html_translate, default="<p></p>")
+
     footer_template_description = fields.Html(
-        'Website Description Footer', sanitize_attributes=False, translate=html_translate, default="<p></p>")   
-    # header_template_description_rendered = fields.Html('Website Description Dynamical Header', sanitize_attributes=False, compute="compute_template_description_rendered")
-    # website_description_footer_rendered = fields.Html('Website Description Dynamical', sanitize_attributes=False, compute="compute_template_description_rendered")
-    # footer_template_description_rendered = fields.Html('Website Description Dynamical Footer', sanitize_attributes=False, compute="compute_template_description_rendered")
-    terms_page = fields.Char('Terms Page')
+        'Website Description Footer', sanitize_attributes=False, translate=html_translate, default="<p></p>")
+
+    website_description = fields.Html(
+        'Website Description', sanitize_attributes=False, translate=html_translate, default="<p></p>")
+
 
     @api.onchange('sale_order_template_id')
     def _onchange_sale_order_template_id(self):
         ret = super(SaleOrder, self)._onchange_sale_order_template_id()
         if self.sale_order_template_id:
             template = self.sale_order_template_id.with_context(lang=self.partner_id.lang)
-            self.website_description_footer = template.website_description_footer
-            self.terms_page = template.terms_page
+            self.website_description = template.website_description
             self.header_template_description = template.header_template_description
             self.footer_template_description = template.footer_template_description
         return ret
+
+    def render_template_field(self, template_html, template_type="template"):
+        if not template_html:
+            return "<p></p>"
+
+        values = {'sale_order': self}
+        template = str(template_html).replace("<br>", "<br/>")
+
+        try:
+            parsed_template = etree.fromstring(f"<div>{template}</div>")
+            rendered = self.env["ir.qweb"]._render(parsed_template, values)
+            return rendered
+        except etree.XMLSyntaxError as e:
+            _logger.error(f"XML parse error in {template_type}: {e}")
+            return f"<p>Error: Malformed HTML in {template_type}</p>"
+        except QWebException as e:
+            _logger.error(f"QWeb render error in {template_type}: {e}")
+            return f"<p>Error: QWeb rendering failed - {e}</p>"
+        except Exception as e:
+            _logger.error(f"Unknown error rendering {template_type}: {e}")
+            return f"<p>Error: {e}</p>"
         
-    # @api.depends("header_template_description","footer_template_description")
-    # def compute_template_description_rendered(self):
-    #     for record in self:
-    #         if record.header_template_description:
-    #             record.header_template_description_rendered = self.template_description_rendererer(record,record.header_template_description,"header")
-    #         else:
-    #             record.header_template_description_rendered = "<p></p>"
-    #
-    #         if record.website_description_footer:
-    #             record.website_description_footer_rendered = self.template_description_rendererer(record,record.website_description_footer,"website description")
-    #         else:
-    #             record.website_description_footer_rendered = "<p></p>"
-    #
-    #         if record.footer_template_description:
-    #             record.footer_template_description_rendered = self.template_description_rendererer(record,record.footer_template_description,"footer")
-    #         else:
-    #             record.footer_template_description_rendered = "<p></p>"
-    #
-    # def template_description_rendererer(self,record,template,template_type):
-    #     values = {'sale_order':record}
-    #     template = str(template).replace("<br>","<br/>")
-    #     try:
-    #         parsed_template = etree.fromstring(template)
-    #         renderd_template = self.env["ir.qweb"]._render(parsed_template,values)
-    #     except etree.XMLSyntaxError as e:
-    #         _logger.error(f"Got this error while trying to parse the sale order template for the {template_type}: {e}. The qweb/html is probably malformed for the template {record.sale_order_template_id.name}.")
-    #         renderd_template = f"<p>Got this error while trying to parse the sale order template for the {template_type}: {e}. The qweb/html is probably malformed for the template {record.sale_order_template_id.name}.</p>"
-    #     except QWebException as e:
-    #         _logger.error(f"Got this error while trying to render the sale order template for the {template_type} with Qweb: {e}. A field is most likely missing in order for the Qweb rendering to work for the template {record.sale_order_template_id.name}.")
-    #         renderd_template = f"<p>Got this error while trying to render the sale order template for the {template_type} with Qweb: {e}. A field is most likely missing in order for the Qweb rendering to work for the template {record.sale_order_template_id.name}.</p>"
-    #     except Exception as e:
-    #         _logger.error(f"Got this unknow error while trying to render the sale order template for the {template_type} with Qweb: {e}")
-    #         renderd_template = f"<p>Got this unknow error while trying to render the sale order template for the {template_type} with Qweb: {e}</p>"
-    #     return renderd_template
--- website_quote_header/models/sale_order_template.py
+++ website_quote_header/models/sale_order_template.py
@@ -6,12 +6,11 @@ from odoo.tools.translate import html_translate
 class SaleOrderTemplate(models.Model):
     _inherit = "sale.order.template"
 
-    website_description_footer = fields.Html(
-        'Website Description Footer', sanitize_attributes=False, translate=html_translate, default="<p></p>") 
+    website_description = fields.Html(
+        'Website Description', sanitize_attributes=False, translate=html_translate, default="<p></p>")
     header_template_description = fields.Html(
         'Website Description Dynamical Header',
         sanitize_attributes=False, translate=html_translate, default="<p></p>")
     footer_template_description = fields.Html(
         'Website Description Dynamical Footer',
         sanitize_attributes=False, translate=html_translate, default="<p></p>")
-    terms_page = fields.Char('Terms Page')
